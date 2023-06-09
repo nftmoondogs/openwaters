5015826117ca026b0500ba8191cad00e1141aaf7
nftmoondogs <nftmoondogs@gmail.com>
2023-06-09 23:45:48 +0530

    kasim

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -26,7 +26,7 @@ const Footer = () => {
                 </div>
               </Link>
               <p className="my-6 dark:text-jacarta-300">
-                Kasim mimo
+                By NFT people, for NFT people
               </p>
 
               {/* <!-- Socials --> */}
